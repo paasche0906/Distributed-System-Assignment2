c03b7831f5daee59126ace8296de5857bd9b3a81
unknown <jiahchengpan@gmail.com>
2025-04-10 13:15:08 +0100

    init project

---
+++ photo-library-app/test/photo-library-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PhotoLibraryApp from '../lib/photo-library-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/photo-library-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PhotoLibraryApp.PhotoLibraryAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
