4f0634b56d5b8a6e0857d357897b769a0b1b0b43
unknown <jiahchengpan@gmail.com>
2025-04-10 13:51:50 +0100

    add LogImage Lambda with SQS and DLQ

--- photo-library-app/lib/photo-library-app-stack.ts
+++ photo-library-app/lib/photo-library-app-stack.ts
@@ -3,6 +3,10 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as sns from 'aws-cdk-lib/aws-sns';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sns_subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class PhotoLibraryAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -24,5 +28,40 @@ export class PhotoLibraryAppStack extends cdk.Stack {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
+
+    // Dead-Letter Queue (DLQ)
+    const deadLetterQueue = new sqs.Queue(this, 'DeadLetterQueue');
+
+    // Main Queue
+    const queue = new sqs.Queue(this, 'MainQueue', {
+      deadLetterQueue: {
+        maxReceiveCount: 1,
+        queue: deadLetterQueue,
+      }
+    });
+
+    // Lambda for logging images
+    const logImageLambda = new lambda.Function(this, 'LogImageLambda', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'log-image.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        TABLE_NAME: imageTable.tableName,
+      }
+    });
+
+    // Grant permissions
+    imageTable.grantWriteData(logImageLambda);
+
+    // Allow Lambda to be triggered by SQS
+    logImageLambda.addEventSourceMapping('LogImageEventSource', {
+      eventSourceArn: queue.queueArn,
+      batchSize: 1,
+    });
+
+    queue.grantConsumeMessages(logImageLambda);
+
+    // SNS Topic subscription to SQS
+    topic.addSubscription(new sns_subs.SqsSubscription(queue));
   }
 }
