1ff3bc64068e8cb6fd64ec47de971eb739e9e1ab
unknown <jiahchengpan@gmail.com>
2025-04-10 13:23:08 +0100

    add S3 bucket, SNS topic, DynamoDB table

--- photo-library-app/lib/photo-library-app-stack.ts
+++ photo-library-app/lib/photo-library-app-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class PhotoLibraryAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Creating an S3 Bucket
+    const imageBucket = new s3.Bucket(this, 'ImageBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PhotoLibraryAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create SNS Topic
+    const topic = new sns.Topic(this, 'ImageTopic', {
+      displayName: 'Image Upload and Metadata Topic'
+    });
+
+    // Creating a DynamoDB Table
+    const imageTable = new dynamodb.Table(this, 'ImageTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
